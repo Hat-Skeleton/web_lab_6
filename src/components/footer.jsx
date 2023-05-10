4308c7c39deb0552720591ecb4461d3767313992
Cody <89228187+Hat-Skeleton@users.noreply.github.com>
2023-05-10 18:15:55 +1200

    Initial commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import "/footer.css";
+
+const Footer = () => {
+  return (
+    <div className="foot-bottom bg-gray-200 py-5 text-gray-900 text-center">
+      <p>Copyright © 2023 CC's. Powered by nicotine & Lift+.</p>
+    </div>
+  );
+};
+
+export default Footer;
